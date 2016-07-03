9a348d58aa750d5e0d35c425c4ef0f2fb082fc47
sujitsarkar0991 <sujitsarkar91@gmail.com>
2016-07-03 13:14:19 +0530

    Commit from Windows

---
+++ Fibonacci.py
@@ -0,0 +1,16 @@
+def fib(n):
+	first = 0
+	second = 1
+	for x in range(n):
+		if(x<=1):
+			print x
+		else:
+			temp = first+second
+			first,second = second,temp
+			print temp
+
+if __name__ == "__main__":
+	fib(6)
+
+
+	
