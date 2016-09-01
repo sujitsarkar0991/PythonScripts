5fcaf169665f76b7c66c23ea5d79c4c41f72edd1
Sujiit  Sarkar <sujitsarkar91@gmail.com>
2016-09-01 15:59:40 +0530

    Added exception handling.

--- YoutubePlaylistDownloader.py
+++ YoutubePlaylistDownloader.py
@@ -1,29 +1,38 @@
 import requests
-from  bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 from pytube import YouTube
+import os
 
 
 def getAllVideoLinks(playlist):
     source_code = requests.get(playlist)
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text, 'html.parser')
-    song_list = []
+    video_list = []
     for link in soup.findAll('a', {'class': 'pl-video-title-link yt-uix-tile-link yt-uix-sessionlink spf-link '}):
-        downloadVideos(r'https://www.youtube.com'+link.get('href'))
+        video_list.append(r'https://www.youtube.com'+link.get('href'))
+    return video_list
 
-def downloadVideos(videoUrl):
-    yt = YouTube(videoUrl)
-    print yt.get_videos()
-    try:
-        video = yt.get('mp4','720p')
-    except Exception as e:
-        video = yt.get('mp4','360p')
-        print e
-    #video.download(r'C:\Users\310168881\Desktop\Videos')
+def downloadVideos(video_url_list):
+    video = None
+    folderName = "YouTubePlaylist"
+    if not os.path.exists(folderName):
+        os.mkdir(folderName)
+    for video_url in video_url_list:
+        try:
+            yt = YouTube(video_url)
+            try:
+                video = yt.get('mp4','720p')
+            except Exception:
+                video = yt.get('mp4','360p')
+        except Exception as e:
+            pass
+        video.download(folderName)
 
 def main():
     playlist = raw_input("Enter the link for playlist: ")
-    getAllVideoLinks(playlist)
+    video_links = getAllVideoLinks(playlist)
+    downloadVideos(video_links)
 
 if __name__ == '__main__':
     main()
